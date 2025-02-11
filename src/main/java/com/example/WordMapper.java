3642f5b91874a62aec9696a912e91df875d9d6f6
Bhanu Saisree Thanniru <80143663+BhanuThanniru@users.noreply.github.com>
2025-02-11 18:34:14 +0000

    initial commit

--- src/main/java/com/example/WordMapper.java
+++ src/main/java/com/example/WordMapper.java
@@ -1,5 +1,18 @@
-package com.example;
+package com.example.controller;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
-public class WordMapper{
-    
+public class WordMapper extends Mapper<Object, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        StringTokenizer itrToken = new StringTokenizer(value.toString());
+        while (itrToken.hasMoreTokens()) {
+            context.write(word, one);
+        }
+    }
 }
